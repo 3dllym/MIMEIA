0e951b9c3e812f84c1a13605e5fa3b6284599820
BellCampa <isaias.abel@hotmail.com>
2025-08-17 09:46:22 -0400

    tgjbkbbb

--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
 // Carpeta de uploads
-const uploadFolder = path.join(__dirname, 'public/uploads');
-if (!fs.existsSync(uploadFolder)) fs.mkdirSync(uploadFolder);
-
-// Configuración de multer
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadFolder);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage });
+const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 // Servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
-// Ruta para subir fotos
-app.post('/upload', upload.single('foto'), (req, res) => {
-    res.json({ filename: req.file.filename });
+// Ruta para subir imágenes
+app.post('/subir', upload.single('foto'), (req, res) => {
+    res.send('¡Foto subida con éxito!');
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:3000`);
 });
